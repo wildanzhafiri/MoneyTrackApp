8482ed7e6e4679fa31b25d462f63fa92e1a2a995
wildan <wildanzhaf@gmail.com>
2025-03-27 07:13:55 +0700

    feat: implement AddTransactionActivity logic

--- app/src/main/java/com/example/moneytrackapp/AddTransactionActivity.java
+++ app/src/main/java/com/example/moneytrackapp/AddTransactionActivity.java
@@ -1,19 +1,55 @@
 package com.example.moneytrackapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class AddTransactionActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private RecyclerView categoryRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
+        EditText etCategory = findViewById(R.id.et_category);
+
+        spinner = findViewById(R.id.spinner_type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.transaction_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        categoryRecycler = findViewById(R.id.categoryRecyclerView);
+        categoryRecycler.setLayoutManager(new GridLayoutManager(this, 3));
+
+        CategoryGridAdapter categoryAdapter = new CategoryGridAdapter(this, CategoryData.getCategories());
+        categoryRecycler.setAdapter(categoryAdapter);
+
+        categoryAdapter.setOnCategoryClickListener(categoryName -> {
+            etCategory.setText(categoryName);
+        });
+
+        Button manageCategoryButton = findViewById(R.id.manage_category_button);
+        manageCategoryButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ManageCategory.class);
+            startActivity(intent);
+        });
+
+        Button manageContinueButton = findViewById(R.id.btn_continue);
+        manageContinueButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TransactionActivity.class);
+            startActivity(intent);
+        });
 
         BottomNavbarView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setActiveIcon(R.id.add_transaction);
