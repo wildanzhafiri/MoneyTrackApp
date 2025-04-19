8f6467e67653d963f4fd4faba34b9060b216baec
wildan <wildanzhaf@gmail.com>
2025-04-20 02:26:18 +0700

    style: reorder import statements in ManageCategoryAdapter

--- app/src/main/java/com/example/moneytrackapp/ManageCategoryAdapter.java
+++ app/src/main/java/com/example/moneytrackapp/ManageCategoryAdapter.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
 public class ManageCategoryAdapter extends RecyclerView.Adapter<ManageCategoryAdapter.ViewHolder> {
