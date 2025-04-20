fca0a4c0bda89a2b793a03bb94174d90d93ac3ae
rafiframadhani <rafiframadhani17@gmail.com>
2025-04-20 12:28:03 +0700

    docs: added a CircleImageView library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.circleimageview)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
